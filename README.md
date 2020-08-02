45e605b4bddfc9e4f39dc806d125800c5a9f2a61
Cesar <cesdia883@gmail.com>
2020-08-02 18:14:30 -0400

    looping through csv_files and returning html tables

--- README.md
+++ README.md
@@ -4,10 +4,18 @@ Convert downloaded CSV files to HTML webpages.
 ## Problem
 Many small companies do not have the know-how or the resources to update their websites constantly. In these conditions, free collaborative products such as google sheets and docs become the go to tools for creating content. The issue arrives when translating this content into HTML for their webpages. They need simple but powerful tools, that specialize in specific jobs. 
 
-
 ## Solution
 CSV-to-HTML requires the user to download the finished Google Sheet as a CSV, upload it and watch as it gets transformed into HTML that they can then upload to their websites. This is especially useful for CMS such as WordPress. 
 
+## Use
+Step 1 - Place your csv files in "CSV_Files" directory
+Step 1 - Look at the "example.html" file found in "templates" directory for an example of how to format your Jinja2 syntax
+Step 2 - Create your html template, saving it in the "templates" directory
+Step 3 - When initializing the class, pass the name of your template as a value(template="example.html")
+Step 4 - Run your file CSVtoHTML.loop_through_csv_files()
+
 ### References:
+https://github.com/vividvilla/csvtotable
 https://github.com/derekeder/csv-to-html-table
 https://github.com/yeskarthik/CSV-to-HTML-report-generator/blob/master/csvtohtmls.py
+https://www.youtube.com/watch?v=lhpNRDGzInw
--- csvtohtml.py
+++ csvtohtml.py
@@ -2,15 +2,17 @@ import os
 import time
 import csv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import webbrowser
 
+project_dir = os.path.dirname(os.path.abspath(__file__))     # Get the projects directory
+templates_dir = os.path.join(project_dir, "templates")       # Get the projects directory
+env = Environment(loader=FileSystemLoader(templates_dir))
 
 class CSVToHTML:
-    def __init__(self, template_name="CSV-to-Html"):
-        self.project_dir = os.path.dirname(os.path.abspath(__file__))       # Get the projects directory
-        self.csv_dir = os.path.join(self.project_dir, "CSV_Files")
+    def __init__(self, template="qedc_table.html"):
+        self.csv_dir = os.path.join(project_dir, "CSV_Files")
         self.csv_file = None
-        self.templates_dir = os.path.join(self.project_dir, "templates")
-        self.template_name = template_name
+        self.template = template
 
     def read_csv(self, csv_file_path):
         """Takes a given csv_file_path and returns the csv.reader object"""
@@ -23,6 +25,7 @@ class CSVToHTML:
             self.csv_file = list_read_csv
 
     def get_headers_and_rows(self, csv_file_path):
+        """Takes the csv render object and returns the headers and rows"""
         csv = self.read_csv(csv_file_path)
 
         csv_headers = [self.csv_file[0]]
@@ -31,42 +34,30 @@ class CSVToHTML:
         #print(csv_headers,"\n\n\n", csv_rows)
         return csv_headers, csv_rows
 
-    def write_to_html(self):
-        i = 1 
-        csv_headers, csv_rows = self.get_headers_and_rows()
-        new_html = self.templates_dir + self.template_name + ".html"
-        f = open(new_html,'w')
-        f.write('<html><head>' + self.template_name + '</head><body><table>')
-        for row in csv_rows:
-            print (each)
-            f.write('<tr><td><b>')
-            f.write(b[j]+':')
-            f.write('</b></td><td>')
-            f.write(each)
-            f.write('</td></tr>')
-            f.write('\n')
-            print('/n')
-            j+=1
-        f.write('</table></body><html>')
-        f.close()
 
-        for header in csv_headers:
-            f.write("<thead>")
-        for row in csv_rows:
-            f.write(thead)
-
+    def csv_to_html(self, csv_file_path):
+        """Returns the formatted html table"""
+        csv = self.get_headers_and_rows(csv_file_path)
+        csv_headers, csv_rows = csv[0], csv[1]
+        data = {
+            "headers": csv_headers,
+            "rows": csv_rows
+        }
+        template = env.get_template(self.template)
+        parsed_template = template.render(data)
+        print(parsed_template)
 
 
     def loop_through_csv_files(self):
+        """Loops through the csv_files directory and returns the formatted html table"""
         with os.scandir(self.csv_dir) as input_file:
             for csv in input_file:
                 if csv != None:
                     # function that actually converts to html
-                    return self.get_headers_and_rows(csv)
+                    return self.csv_to_html(csv)
                 else:
                     continue
 
 
 csv_to_html = CSVToHTML()
 csv = csv_to_html.loop_through_csv_files()
-
